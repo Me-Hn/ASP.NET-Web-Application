c9c4e7c8ba65ec27ffcc0d879b1402b74fdbe9d6
wasi010 <wasif442239@gmail.com>
2025-01-28 10:41:53 +0500

    deleting work

--- WebApplication1/connection.aspx
+++ WebApplication1/connection.aspx
@@ -15,7 +15,26 @@
             <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
             <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
             <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Button" />
+        
+        
         </p>
+<asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="False" 
+    OnRowDeleting="GridView1_RowDeleting" 
+    OnRowEditing="GridView1_RowEditing"
+    OnRowUpdating="GridView1_RowUpdating" 
+    OnRowCancelingEdit="GridView1_RowCancelingEdit"
+    DataKeyNames="id">
+    <Columns>
+        <asp:BoundField DataField="id" HeaderText="ID" />
+        <asp:BoundField DataField="name" HeaderText="Name" />
+        <asp:BoundField DataField="email" HeaderText="Email" />
+        <asp:CommandField ShowDeleteButton="True" />
+         <asp:CommandField ShowEditButton="True" />
+    </Columns>
+</asp:GridView>
+
+
+        
     </form>
 </body>
 </html>
--- WebApplication1/connection.aspx.cs
+++ WebApplication1/connection.aspx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
@@ -10,14 +11,31 @@ namespace WebApplication1
 {
     public partial class connection : System.Web.UI.Page
     {
+        string connstring = "Data Source=DESKTOP-FG19520;Initial Catalog=aptech;Integrated Security=True";
+
+        public void fetchData()
+        {
+            SqlConnection conn = new SqlConnection(connstring);
+
+            string _data = "select * from office"; 
+            SqlDataAdapter adapter = new SqlDataAdapter(_data,conn);
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+
+
+        }
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            if (!IsPostBack)
+            {
+                fetchData();
+            }
         }
 
         protected void Button1_Click(object sender, EventArgs e)
         {
-            string connstring = "Data Source=DESKTOP-QOSO84J\\SQLEXPRESS01;Initial Catalog=office;Integrated Security=True";
             SqlConnection conn = new SqlConnection(connstring);
 
             string val1 = TextBox1.Text;
@@ -41,6 +59,47 @@ namespace WebApplication1
             {
                 conn.Close();
             }
+
+
+        }
+
+        protected void GridView1_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            // Retrieve the primary key of the row to be deleted
+            int id = Convert.ToInt32(GridView1.DataKeys[e.RowIndex].Value);
+
+
+            // Perform the delete operation
+            SqlConnection conn = new SqlConnection(connstring);
+
+            string deleteQuery = "DELETE FROM office WHERE id = @id";
+                SqlCommand cmd = new SqlCommand(deleteQuery, conn);
+
+                // Use parameters to prevent SQL injection
+                cmd.Parameters.AddWithValue("@id", id);
+
+                conn.Open();
+                cmd.ExecuteNonQuery();
+                conn.Close();
+            
+
+            // Rebind the GridView to refresh the data
+            fetchData();
+        }
+
+        protected void GridView1_RowEditing(object sender, GridViewEditEventArgs e)
+        {
+
+        }
+
+        protected void GridView1_RowUpdating(object sender, GridViewUpdateEventArgs e)
+        {
+
+        }
+
+        protected void GridView1_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
+        {
+
         }
     }
 }
